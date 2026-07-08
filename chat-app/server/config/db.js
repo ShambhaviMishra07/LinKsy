95353fc4b50a39cfa5f15dc9ba7f3eb7c82e1e1f
Shambhavi Mishra <shambhavim171@gmail.com>
2026-07-08 23:02:42 +0530

    modified db.js

--- chat-app/server/config/db.js
+++ chat-app/server/config/db.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Mongo URI:", process.env.MONGO_URI);
+    
 
     await mongoose.connect(process.env.MONGO_URI);
 
